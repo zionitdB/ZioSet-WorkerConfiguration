e1638aa29987742d3b962879a2589a74707167cc
SumitKeshniya <sumit.keshniya@zionit.in>
2026-01-12 14:54:09 +0530

    active-modules-list-api

--- backend/src/main/java/com/ZioSet_WorkerConfiguration/rolepermission/repository/ModuleRepository.java
+++ backend/src/main/java/com/ZioSet_WorkerConfiguration/rolepermission/repository/ModuleRepository.java
@@ -3,6 +3,10 @@ package com.ZioSet_WorkerConfiguration.rolepermission.repository;
 import com.ZioSet_WorkerConfiguration.rolepermission.model.ModulePermission;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ModuleRepository extends JpaRepository<ModulePermission,Integer> {
     boolean existsByModuleName(String moduleName);
+
+    List<ModulePermission> findByActiveTrue();
 }
