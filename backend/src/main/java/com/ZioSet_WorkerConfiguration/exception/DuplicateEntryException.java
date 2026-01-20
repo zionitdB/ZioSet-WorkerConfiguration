7bcfad5bac42ffa542d80caf45cd59abb269b0f6
SumitKeshniya <sumit.keshniya@zionit.in>
2026-01-20 11:49:41 +0530

    added-exception-handler

---
+++ backend/src/main/java/com/ZioSet_WorkerConfiguration/exception/DuplicateEntryException.java
@@ -0,0 +1,7 @@
+package com.ZioSet_WorkerConfiguration.exception;
+
+public class DuplicateEntryException extends RuntimeException {
+    public DuplicateEntryException(String message) {
+        super(message);
+    }
+}
